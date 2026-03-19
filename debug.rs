563b0284b2369f789f241d5819090ac71eef4a68
Troy <troy.travlos@gmail.com>
2026-03-19 10:46:30 +0100

    chore: remove temporary debug scripts

--- debug.rs
+++
@@ -1,8 +0,0 @@
-use nattydate::{ParseConfig, tokenize_and_classify};
-
-fn main() {
-    let mut config = ParseConfig::default();
-    config.debug = true;
-    let tokens = tokenize_and_classify("7/6/26", &config);
-    println!("{:#?}", tokens);
-}
